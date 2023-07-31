950b1255784975d35c6522f2ad348459ee1f5363
Pamela <Pamelat00@hotmail.com>
2023-07-31 18:19:53 +0100

    Refactoring and also added fraud check functionality

--- customer/src/main/java/org/customerregistrationservice/customer/CustomerService.java
+++ customer/src/main/java/org/customerregistrationservice/customer/CustomerService.java
@@ -1,9 +1,13 @@
 package org.customerregistrationservice.customer;
 
+import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
 
 @Service
-public record CustomerService(CustomerRepository customerRepository) {
+@AllArgsConstructor
+public class CustomerService {
+
+    private CustomerRepository customerRepository;
     public void registerCustomer(CustomerRegistrationRequest customerRequest) {
         Customer customer = Customer.builder()
                 .firstName(customerRequest.firstName())
